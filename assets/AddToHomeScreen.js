8a963ecfeefc85454deecd437ba63638dfe1cb02
KRoperUK <13541920+KRoperUK@users.noreply.github.com>
2023-05-22 21:09:50 +0100

    Changed safari pop-up to position fixed

--- assets/AddToHomeScreen.js
+++ assets/AddToHomeScreen.js
@@ -6,7 +6,7 @@ template.innerHTML = `
 <style>
 :host {
   display: none;
-  position: absolute;
+  position: fixed;
   bottom: 20px;
   left: 50%;
   transform: translateX(-50%);
@@ -87,8 +87,8 @@ export default class AddToHomeScreen extends HTMLElement {
     const webkit = !!ua.match(/WebKit/i);
     const iOSSafari = iOS && webkit && !ua.match(/CriOS/i);
 
-    if (iOSSafari && !navigator.standalone) {
-    // if (true) {
+    // if (iOSSafari && !navigator.standalone) {
+    if (true) {
       this.style.display = 'inline-block';
     }
   }
